c2b7f3237706c67b63af67a0a1848dc8fbc2e31f
VicenJH <vjimenezhincapie@gmail.com>
2025-08-11 21:06:31 -0500

    Vicen

--- src/main/java/co/edu/uniquindio/poo/veterinaria/Model/ConsultaMedica.java
+++ src/main/java/co/edu/uniquindio/poo/veterinaria/Model/ConsultaMedica.java
@@ -3,7 +3,15 @@ package co.edu.uniquindio.poo.veterinaria.Model;
 public class ConsultaMedica {
     private Agenda agenda;
     private String diagnostico;
-    
+    private String tratamiento;
+
+    public ConsultaMedica(AgendaCitaMedica agendacitamedica, String diagnostico,  String tratamiento) {
+        this.agendacitamedica = agendacitamedica;
+        this.diagnostico = diagnostico;
+        this.tratamiento = tratamiento;
+
+
+    }
 
 
 }
