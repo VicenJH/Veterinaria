df1bd747fe158e717df70ee9d4b2376dc71a61a7
VicenJH <vjimenezhincapie@gmail.com>
2025-08-11 19:56:46 -0500

    Primer commit

---
+++ src/main/java/co/edu/uniquindio/poo/veterinaria/Model/Veterinario.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.poo.veterinaria.Model;
+
+public class Veterinario extends Persona{
+
+
+    public Veterinario(String nombre, String apellido, String telefono, String direccion,String idProfesional) {
+        super(nombre, apellido, telefono, direccion);
+
+    }
+}
