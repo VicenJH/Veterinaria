2fc34d32fb0497533dfccb5671f5ea96e653041f
Isabel Angulo <isabelf.angulom@uqvirtual.edu.co>
2025-08-12 22:13:52 -0500

    Feat: Ajustes en algunos metodos en la clases Propietario y Veterinaria

--- src/main/java/co/edu/uniquindio/poo/veterinaria/Model/Propietario.java
+++ src/main/java/co/edu/uniquindio/poo/veterinaria/Model/Propietario.java
@@ -4,8 +4,9 @@ import java.util.LinkedList;
 
 public class Propietario extends Persona    {
      LinkedList<Mascota>mascotaspropias;
+    private String idPropietario;
 
-    public Propietario(String nombre, String apellido, String telefono, String direccion) {
+    public Propietario(String nombre, String apellido, String telefono, String direccion, String idPropietario) {
         super(nombre, apellido, telefono, direccion);
         this.mascotaspropias=new LinkedList<>();
 
@@ -16,5 +17,17 @@ public class Propietario extends Persona    {
 
     public void setMascotaspropias(LinkedList<Mascota> mascotaspropias) {
         this.mascotaspropias = mascotaspropias;
+    }
+
+    public LinkedList<Mascota> getMascotaspropias() {
+        return mascotaspropias;
+    }
+
+    public String getIdPropietario() {
+        return idPropietario;
+    }
+
+    public void setIdPropietario(String idPropietario) {
+        this.idPropietario = idPropietario;
     }
 }
--- src/main/java/co/edu/uniquindio/poo/veterinaria/Model/Veterinaria.java
+++ src/main/java/co/edu/uniquindio/poo/veterinaria/Model/Veterinaria.java
@@ -7,15 +7,15 @@ import java.util.List;
 public class Veterinaria {
     String nombre;
     String direccion;
-    LinkedList <HistoriaClinica> ListahistoriasClinicas;
+    LinkedList<HistoriaClinica> ListahistoriasClinicas;
     LinkedList<Mascota> listademascotasenclinica;
-    LinkedList<Veterinario>veteriniosclinica;
-    LinkedList<Propietario>propietariosdemascotas;
-    LinkedList<PersonalApoyo>PersonalApoyo;
+    LinkedList<Veterinario> veteriniosclinica;
+    LinkedList<Propietario> propietariosdemascotas;
+    LinkedList<PersonalApoyo> PersonalApoyo;
     private LinkedList<AgendaCitaMedica> citas;
 
-//constructor de la clase Veterinaria
-    public  Veterinaria(String nombre, String direccion) {
+    //constructor de la clase Veterinaria
+    public Veterinaria(String nombre, String direccion) {
         this.nombre = nombre;
         this.direccion = direccion;
         this.ListahistoriasClinicas = new LinkedList<>();
@@ -24,16 +24,20 @@ public class Veterinaria {
         this.PersonalApoyo = new LinkedList<>();
         this.listademascotasenclinica = new LinkedList<>();
     }
+
     // Getters y Setters
     public String getNombre() {
         return nombre;
     }
+
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
     public String getDireccion() {
         return direccion;
     }
+
     public void setDireccion(String direccion) {
         this.direccion = direccion;
     }
@@ -87,16 +91,16 @@ public class Veterinaria {
     }
 
     //Metodo para verificar si por el numero de telefono ya esta registrado el propietario
-    public boolean verificarPropietario(String telefono) {
+    public boolean verificarPropietario(String idPropietario) {
         for (Propietario propietario : propietariosdemascotas) {
-            if (propietario.getTelefono().equals(telefono)) {
+            if (propietario.getTelefono().equals(idPropietario)) {
                 return false;
             }
         }
         return true;
     }
 
-//Metodo que verifica si el veterinario ya esta registrado por su id
+    //Metodo que verifica si el veterinario ya esta registrado por su id
     public boolean verificarVeterianario(String idProfesional) {
         for (Veterinario veterinario : veteriniosclinica) {
             if (veterinario.getIdProfesional().equals(idProfesional)) {
@@ -108,14 +112,25 @@ public class Veterinaria {
 
     // Metodo para registrar un propietario
     public boolean registrarPropietario(Propietario propietario) {
-        if (verificarPropietario(propietario.getTelefono())) {
+        if (verificarPropietario(propietario.getIdPropietario())) {
             propietariosdemascotas.add(propietario);
             return true;
         }
         return false;
     }
 
-// Metodo para registrar un veterinario
+    // metodo para eliminar un propietario por id
+    public boolean eliminarPropietario(String idPropietario) {
+        for (Propietario propietario : propietariosdemascotas) {
+            if (propietario.getTelefono().equals(idPropietario)) {
+                propietariosdemascotas.remove(propietario);
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // Metodo para registrar una mascota
     public boolean registrarMascota(Mascota mascota) {
         for (Mascota NuevaMascota : listademascotasenclinica) {
             if (NuevaMascota.getId() == mascota.getId()) {
@@ -125,7 +140,20 @@ public class Veterinaria {
         listademascotasenclinica.add(mascota);
         return true;
     }
-// Metodo para registrar un veterinario
+
+    // Metodo para eliminar una mascota por id
+    public boolean eliminarMascota(String idMascota) {
+        for (Mascota mascota : listademascotasenclinica) {
+            if (String.valueOf(mascota.getId()).equals(idMascota)) {
+                listademascotasenclinica.remove(mascota);
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // Metodo para registrar un veterinario
     public boolean registrarVeterinario(Veterinario veterinario) {
         if (verificarVeterianario(veterinario.getIdProfesional())) {
             veteriniosclinica.add(veterinario);
@@ -133,13 +161,25 @@ public class Veterinaria {
         }
         return false;
     }
+
+    // Metodo para eliminar un veterinario por id
+    public boolean eliminarVeterinario(String idVeterinario) {
+        for (Veterinario veterinario : veteriniosclinica) {
+            if (veterinario.getIdProfesional().equals(idVeterinario)) {
+                veteriniosclinica.remove(veterinario);
+                return true;
+            }
+        }
+        return false;
+    }
+
     //metodo para asignar un id ramdon a la mascota
     public void asignarIdMascota() {
         for (Mascota mascota : listademascotasenclinica) {
             if (mascota.getId() == 0) {
                 int idmascota;
                 do {
-                    idmascota = (int)(Math.random() * 9000000) + 1000000;
+                    idmascota = (int) (Math.random() * 9000000) + 1000000;
                 } while (idYaExiste(idmascota));
 
                 mascota.setId(idmascota);
@@ -147,13 +187,14 @@ public class Veterinaria {
             }
         }
     }
-// Metodo para agendar una nueva cita
+
+    // Metodo para agendar una nueva cita
     public void agendarCita(AgendaCitaMedica cita) {
         citas.add(cita);
     }
 
     // Metodo para buscar una dia por una fecha en especifico
-    public List<AgendaCitaMedica> buscarCitaPorDia(LocalDate fechaBuscada){
+    public List<AgendaCitaMedica> buscarCitaPorDia(LocalDate fechaBuscada) {
         List<AgendaCitaMedica> citaResultado = new LinkedList<>();
         for (AgendaCitaMedica cita : citas) {
             if (cita.getFecha().equals(fechaBuscada)) {
@@ -172,19 +213,27 @@ public class Veterinaria {
         return false;
     }
 
-public HistoriaClinica buscarHistoriaClinica(int idMascota){
-        for(HistoriaClinica historiaClinica: ListahistoriasClinicas){
-            if(historiaClinica.getMascota().getId() == idMascota){
+    public HistoriaClinica buscarHistoriaClinica(int idMascota) {
+        for (HistoriaClinica historiaClinica : ListahistoriasClinicas) {
+            if (historiaClinica.getMascota().getId() == idMascota) {
                 return historiaClinica;
             }
         }
         return null;
-}
+    }
 
+    public Mascota buscarMascota(int idMascota) {
+        for (Mascota mascota : listademascotasenclinica) {
+            if (mascota.getId() == idMascota) {
+                return mascota;
+            }
+        }
+        return null;
 
-
-
-
+    }
+    public void eliminarMascota(Mascota mascota) {
+        listademascotasenclinica.remove(mascota);
+    }
 }
 
 
